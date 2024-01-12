b0b3c613c8716f70eeb3eab3eedd5d7e52152e5d
Lunamoon36 <150391744+Lunamoon36@users.noreply.github.com>
2024-01-12 19:28:48 +0000

    Created index.js file and added code

---
+++ src/index.js
@@ -0,0 +1,53 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+
+  let city = cityInput.value;
+  let apiKey = "773b1fb672t5o4902c27a62f4db0b82a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let cityForm = document.querySelector("#city-form");
+cityForm.addEventListener("submit", showCity);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
